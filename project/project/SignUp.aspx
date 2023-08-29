680560ebca08905a0e3e5575206636d65727727e
Kevan <msjsci2021ema210@gmail.com>
2023-08-29 09:09:48 +0530

    First commit

---
+++ project/project/SignUp.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SignUp.aspx.cs" Inherits="project.SignUp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">Name</td>
+                <td>
+                    <asp:TextBox ID="tbName" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="tbName" ErrorMessage="Name is Required" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Email</td>
+                <td>
+                    <asp:TextBox ID="tbEmail" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="tbEmail" ErrorMessage="Email is Required" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">UserName</td>
+                <td>
+                    <asp:TextBox ID="tbUName" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidatorUserName" runat="server" ControlToValidate="tbUName" ErrorMessage="UserName is Required" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Password</td>
+                <td>
+                    <asp:TextBox ID="tbPassword" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidatorPassword" runat="server" ControlToValidate="tbPassword" ErrorMessage="Password is Required" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnRegister" runat="server" Text="Register" Width="65px" OnClick="btnregistr_Click" />
+                </td>
+                <td>&nbsp;
+                                    </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ project/project/SignUp.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace project
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+        DataSet ds;
+        protected void btnregister_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Entered");
+            Response.Write("Hello");
+            /*
+            string constr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\msjsc\\Desktop\\WAD\\project\\project\\project\\App_Data\\Database.mdf;Integrated Security=True";
+            try
+            {
+                SqlConnection con = new SqlConnection(constr);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM User";
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            ds = new DataSet();
+
+
+                using (con)
+                {
+                    con.Open();
+                    da.Fill(ds, "User");
+                    //DataTable dt = ds.Tables["User"];
+                   // DataRow dr = dt.NewRow();
+                    //dr["Name"] = tbName.Text;
+                    //dr["UserName"] = tbUName.Text;
+                    //dr["Email"] = tbEmail.Text;
+                    //dr["Password"] = tbPassword.Text;
+                    //dt.Rows.Add(dr);
+                    //SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(da);
+                    //da.Update(ds, "User");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("In catch");
+                Console.WriteLine(ex.Message);
+            }
+            //GridView1.DataSource = ds.Tables["User"];
+            //GridView1.DataBind();
+            */
+        }
+
+        protected void btnregistr_Click(object sender, EventArgs e)
+        {
+            Trace.Write("Hello");
+            Response.Write("Hum sath");
+            string constr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\msjsc\\Desktop\\WAD\\project\\project\\project\\App_Data\\Database.mdf;Integrated Security=True";
+            try
+            {
+                SqlConnection con = new SqlConnection(constr);
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT * FROM Person";
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                ds = new DataSet();
+                using (con)
+                {
+       
+                    con.Open();
+                    da.Fill(ds, "Person");
+                    DataTable dt = ds.Tables["Person"];
+                    DataRow dr = dt.NewRow();
+                    dr["Name"] = tbName.Text;
+                    dr["UserName"] = tbUName.Text;
+                    dr["Email"] = tbEmail.Text;
+                    dr["Password"] = tbPassword.Text;
+                    dt.Rows.Add(dr);
+                    SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(da);
+                    da.Update(ds, "Person");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+        }
+    }
+}
